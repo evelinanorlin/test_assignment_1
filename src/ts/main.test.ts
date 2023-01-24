688f5aeb709919c1b698bded3eec320994d87409
Evelina Norlin <evelina.norlin@hotmail.com>
2023-01-24 15:53:21 +0100

    structured the testing file

---
+++ src/ts/main.test.ts
@@ -0,0 +1,42 @@
+import { Todo } from "./models/Todo";
+import { createNewTodo } from "./main";
+
+
+
+// ANONYM FUNKTION 1
+// förväntad funktion: Alla todos försvimner när en trycker på "rensa lista"
+
+
+// ANONYM FUNKTION 2
+// förväntad funktion: Antingen visas todon i listan, eller så visas ett error.
+// körs när en trycker på "Skapa"
+
+
+// FUNKTION 3  function createNewTodo(todoText: string, todos: Todo[])
+// förväntad funktion: kollar om det är fler än 3 bokstäver, antingen pushar in i listan
+// eller visar error
+
+test("should push in to list or throw error", () => {
+// Arrange
+let todoText = 'Köpa mjölk';
+let isDone = false;
+let todos: Todo[] = [new Todo("Promenera hunden", true)];
+
+// Act
+
+// Assert
+
+})
+
+// FUNKTION 4   createHtml(todos: Todo[])
+// förväntad funktion: skapar tom container för att särefter skicka in alla items i listan
+// är item klickad är den överstruken, lägger även till eventlistener på alla li för att ev stryka över
+
+// FUNKTION 5 toggleTodo(todo: Todo)
+// texten stryks över vid klick, nu html skapas (createHtml)
+
+//FUNKTION 6   displayError(error: string, show: boolean)
+// förväntad funktion: visar error-texten genom att lägga till klass
+
+// FUNKTION 7 clearTodos(todos: Todo[])
+// förväntad funktion: tar bort alla items i todo-listan. 
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,12 +1,16 @@
 import { addTodo, changeTodo, removeAllTodos } from "./functions";
 import { Todo } from "./models/Todo";
 
+
+// skapar lista med items som stoppas in i listan i appen
 let todos: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]");
 
+// tar bort alla todos när man trycker på "rensa lista"
 document.getElementById("clearTodos")?.addEventListener("click", () => {
   clearTodos(todos);
 });
 
+// tar texten i input och kör igång createNewTodo när en trycker på "skapa"-knappen
 (document.getElementById("newTodoForm") as HTMLFormElement)?.addEventListener(
   "submit",
   (e: SubmitEvent) => {
@@ -21,7 +25,9 @@ document.getElementById("clearTodos")?.addEventListener("click", () => {
   }
 );
 
-function createNewTodo(todoText: string, todos: Todo[]) {
+// pushar in todon i listan och kör createHtml() om allt stämmer. Måste vara
+// 3 bokstäver minst
+export function createNewTodo(todoText: string, todos: Todo[]) {
   let result = addTodo(todoText, todos);
 
   if (result.success) {
@@ -31,6 +37,7 @@ function createNewTodo(todoText: string, todos: Todo[]) {
   }
 }
 
+// Skapar UL med alla todosen i listan.
 function createHtml(todos: Todo[]) {
   localStorage.setItem("todos", JSON.stringify(todos));
 
@@ -57,11 +64,14 @@ function createHtml(todos: Todo[]) {
   }
 }
 
+// Gör texten överstyken
 function toggleTodo(todo: Todo) {
-  changeTodo(todo);
-  createHtml(todos);
+  changeTodo(todo);   // boolean, changes to flse or true
+  createHtml(todos); // skapar html av listan, lägger till klassen todo__text--done om den är klickad på
 }
 
+// skapar sträng under input om det är för få bokstäver, genom att lägga till eller ta bort klassen show.
+// "error-texten" genereras i addTodo()
 function displayError(error: string, show: boolean) {
   let errorContainer: HTMLDivElement = document.getElementById(
     "error"
@@ -76,9 +86,12 @@ function displayError(error: string, show: boolean) {
   }
 }
 
+
+//tar bort alla todos och genererar ny HTML 
 function clearTodos(todos: Todo[]) {
   removeAllTodos(todos);
   createHtml(todos);
 }
 
+//antar att denna könner vad so ligger lagrat när sidan laddas in
 createHtml(todos);
